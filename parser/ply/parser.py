3cb267d443ea2f76618b3d2afd60b36d38e23f13
dhh1128 <dhardman@adaptivecomputing.com>
2013-07-25 02:48:13 -0600

    first version of ply-based parser; just a lexer at this point, and very primitive

---
+++ parser/ply/parser.py
@@ -0,0 +1,81 @@
+import ply.lex as lex
+
+# List of token names.
+tokens = (
+    'FUNC',
+    'CLASS',
+    'IF',
+    'FOR',
+    'USE',
+    'AS',
+    'ID',
+    'ASSIGN',
+    'DOT',
+    'NUMBER',
+    'COLON',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'CPP_COMMENT',
+    'LPAREN',
+    'RPAREN',
+    'LANGL',
+    'RANGL',
+    'LBRACK',
+    'RBRACK',
+    'STRING',
+)
+
+# Regular expression rules for simple tokens. These are in precedence order.
+t_FUNC = r'func'
+t_CLASS = r'class'
+t_IF = r'if'
+t_FOR = r'for'
+t_USE = r'use'
+t_AS = r'as'
+t_ASSIGN = r'='
+t_DOT = '\.'
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_CPP_COMMENT = r'//.*?(?=\n)'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_LANGL  = r'<'
+t_RANGL  = r'>'
+t_LBRACK  = r'\['
+t_RBRACK  = r'\]'
+t_COLON = r':'
+t_ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_STRING = r'".*?"'
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)    
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):
+    print "Illegal character '%s'" % t.value[0]
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+if __name__ == '__main__':
+    import sys
+    with open(sys.argv[1], 'r') as f:
+        txt = f.read()
+    lexer.input(txt)
+    for token in lexer:
+        print token
